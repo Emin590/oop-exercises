d3c9c1df92c101a0e4339749053ea2dcd04b8081
Emin590 <demirtasemin136@gmail.com>
2024-10-23 13:06:13 +0200

    gameoflife plan

---
+++ basicProgramming/gameOfLife/Program.cs
@@ -0,0 +1,24 @@
+﻿class Game {
+	
+	static void Main(string[] args) {
+		
+		//Check antal naboer funktion for hver celle
+			//Vælg en celle kør igennem som hver row og columns
+			//Tjek for cellens rowx columnx+1, rowx columnx-1, rowx+1 columnx+1, rowx+1 columnx-1, rowx+1 columnx-1, rowx-1 columnx, rowx+1 columnx, rowx-1 columnx+1, rowx-1 column-1 
+			//Sum af af 1 og 0 for omkringliggende celler så man har antal levende celler omkring en
+		//Reproduce, Dø eller Overlev alt efter return værdi fra check-funktion
+			
+		
+		//
+		
+	}
+	
+	
+}
+
+/*
+	Frameloop
+	Check antal naboer <2, 2, 3, +3
+	
+	
+*/
